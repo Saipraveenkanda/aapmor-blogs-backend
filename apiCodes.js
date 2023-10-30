fbb7fda27996873ed99509c995b028d86e62b365
Bharath <dbbk2002@gmail.com>
2023-10-30 18:03:41 +0530

    some changes in login and register api

--- apiCodes.js
+++ apiCodes.js
@@ -1,18 +1,14 @@
 const express = require("express");
-
 const bcrypt = require("bcrypt");
-
 const jwt = require("jsonwebtoken");
 const app = express();
-
 const { MongoClient } = require("mongodb");
-
 const client = new MongoClient("mongodb://localhost:27017");
 
 // Register API
 
 app.post("/register", async (request, response) => {
-  const { firstName, lastName, email, password } = request.body;
+  const { firstname, lastname, email, password, isEmployee } = request.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   client
     .db("Blogsdata")
@@ -24,17 +20,18 @@ app.post("/register", async (request, response) => {
           .db("Blogsdata")
           .collection("users")
           .insertOne({
-            firstName: firstName,
-            lastName: lastName,
+            firstname: firstname,
+            lastname: lastname,
             email: email,
             password: hashedPassword,
+            isEmployee: isEmployee,
           })
           .then((res) => {
             console.log(res);
-            response.send("User created successfully");
+            response.json({ message: "User created successfully" });
           });
       } else {
-        response.json({ error_msg: "email already exists" });
+        response.json({ message: "Email already Exists" });
       }
     });
 });
@@ -43,29 +40,32 @@ app.post("/register", async (request, response) => {
 
 app.post("/login", async (request, response) => {
   const { email, password } = request.body;
-  let dbUser = client
+  client
     .db("Blogsdata")
     .collection("users")
-    .findOne({ email: email });
-  if (dbUser === null) {
-    response.status(400);
-
-    response.json({ error_msg: "Invalid email" });
-  } else {
-    const isPassowrdMatched = await bcrypt.compare(password, dbUser.password);
-
-    if (isPassowrdMatched === true) {
-      const payLoad = {
-        email: email,
-      };
-
-      const jwtToken = jwt.sign(payLoad, "MY_SECRET_TOKEN");
-      response.send(jwtToken);
-    } else {
-      response.status(400);
-      response.json({ message: "Invalid Password" });
-    }
-  }
+    .findOne({ email: email })
+    .then(async (respObj) => {
+      if (dbUser === null) {
+        response.status(400);
+        response.json({ message: "Invalid email" });
+      } else {
+        const isPasswordMatched = await bcrypt.compare(
+          password,
+          dbUser.password
+        );
+        if (isPasswordMatched === true) {
+          const payload = {
+            email: email,
+          };
+          const jwt_token = jwt.sign(payload, "SECRET");
+          response.json({ jwt_token });
+        } else {
+          response.status(400);
+          response.json({ message: "Invalid Password" });
+        }
+      }
+    })
+    .catch((err) => response.send(err));
 });
 
 module.exports = app;
