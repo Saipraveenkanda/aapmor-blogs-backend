c94c69d64648370dd2ba896d82263cd30cdf876e
hariyadav9963 <hjavoji@gmail.com>
2023-10-31 14:30:42 +0530

    added api

--- apiCodes.js
+++ apiCodes.js
@@ -7,7 +7,7 @@ const { connection } = require("./connect");
 
 // Register API
 
-app.post("/register", async (request, response) => {
+app.post("/api/register", async (request, response) => {
   const { firstname, lastname, email, password, isEmployee } = request.body;
   console.log(password);
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -34,7 +34,7 @@ app.post("/register", async (request, response) => {
 
 // Login API
 
-app.post("/login", async (request, response) => {
+app.post("/api/login", async (request, response) => {
   const { email, password } = request.body;
   connection
     .findOne({ email: email })
