e407814dee278e5ca785fcde1bd4774cfec482c0
Nagendrakanda <praveensaik@aapmor.com>
2025-03-23 14:42:30 +0530

    Reversing blogs

--- connections/routes.js
+++ connections/routes.js
@@ -77,8 +77,9 @@ app.put("/users", async (request, response) => {
 
 app.get("/blogs", async (request, response) => {
   const blogsArray = await Model.find({}, { html: 0 });
+  const blogs = blogsArray.reverse();
   try {
-    response.send(blogsArray);
+    response.send(blogs);
   } catch (error) {
     response.send(error);
   }
